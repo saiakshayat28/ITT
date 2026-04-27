522c58e3d07e7f7c1de6dd7b40e5c91f04f4b3d7
saiakshayat28 <saiakshayat28@gmail.com>
2026-04-27 11:35:38 +0530

    Create filecalled.c

---
+++ filecalled.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+   FILE*fptr;
+   char name[20];
+   int age;
+   float salary;
+   /*open for writing*/
+   fptr=fopen("emp.rec","w");
+   if(fptr==NULL)
+   {
+      printf("file does not exists\n");
+      return;
+   }
+   printf("Enter the name\n");
+   scanf("%s",name);
+   fprintf(fptr,"Name=%s\n",name);
+   printf("Enter the age\n");
+   scanf("%d",&age);
+   fprintf(fptr,"Age=%d\n",age);
+   printf("Enter the salary\n");
+   scanf("%f",&salary);
+   fprintf(fptr,"Salary=%f\n",salary);
+   fclose(fptr);
+}
